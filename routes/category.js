c57ce1aa5346d6e8c9d8ba3ae8b15a881f106665
JASantacruz00 <jsantacgdev@gmail.com>
2024-10-19 02:07:17 +0200

    Filter results

--- routes/category.js
+++ routes/category.js
@@ -1,17 +1,21 @@
 const express = require('express');
-const Category = require('../models/Category');
 const Categoria = require('../models/Category');
 const router = express.Router();
 
 router.get('/categories', async (req, res) => {
   try {
-    let category = await Category.find();
-    console.log('Documentos encontrados:', category);
     let category = await Categoria.find();
     if (!category) return res.status(404).json({ msg: 'No existen categorias' });
 
-    res.status(200).json({ msg: 'Operacion procesada correctamente.', categories: [...category] });
+    const resultado = category.map(doc => ({
+      codigo: doc.codigo,
+      nombre_categoria: doc.nombre_categoria
+    }));
+
+    res.status(200).json({ msg: 'Operacion procesada correctamente.', categories: resultado });
+
   } catch (err) {
+
     res.status(500).json({ msg: 'Error al buscar las categorias.' });
     console.log(err);
   }
